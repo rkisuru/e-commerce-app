9cace4680d94ca3c8b5d785781800bdada537de7
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-22 09:44:35 +0530

    implement createCustomer service

---
+++ services/customer/src/main/java/com/rkisuru/customer/customer/CustomerService.java
@@ -0,0 +1,17 @@
+package com.rkisuru.customer.customer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    public String createCustomer(CustomerRequest request) {
+        var customer = customerRepository.save(customerMapper.toCustomer(request));
+        return customer.getId();
+    }
+}
