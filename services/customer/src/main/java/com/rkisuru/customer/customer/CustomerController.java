5c2a8bab82cbf89951baba6831484fa476a8b1b0
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-22 09:45:14 +0530

    feat: implement createCustomer endpoint

---
+++ services/customer/src/main/java/com/rkisuru/customer/customer/CustomerController.java
@@ -0,0 +1,25 @@
+package com.rkisuru.customer.customer;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<String> createCustomer(
+            @RequestBody @Valid CustomerRequest request
+    ) {
+        return ResponseEntity.ok(customerService.createCustomer(request));
+    }
+
+}
