184c151fca4b89d7102fec5d35b2355b74f8f981
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-25 00:14:35 +0530

    implement null check

--- services/customer/src/main/java/com/rkisuru/customer/customer/CustomerMapper.java
+++ services/customer/src/main/java/com/rkisuru/customer/customer/CustomerMapper.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 public class CustomerMapper {
     public Customer toCustomer(CustomerRequest request) {
 
+        if (request == null) {
+            return null;
+        }
         return Customer.builder()
                 .id(request.id())
                 .firstname(request.firstname())
