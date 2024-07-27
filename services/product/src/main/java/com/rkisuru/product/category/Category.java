bba24406bb8713cc09bd92caf937cb5c25bd1350
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-27 23:45:00 +0530

    implement Category entity

---
+++ services/product/src/main/java/com/rkisuru/product/category/Category.java
@@ -0,0 +1,27 @@
+package com.rkisuru.product.category;
+
+import com.rkisuru.product.product.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
