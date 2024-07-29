d7cf1136a1d0b8c17d86106bca635a65c26d25e6
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-29 22:47:06 +0530

    fix: rename request mapping

--- services/product/src/main/java/com/rkisuru/product/product/ProductController.java
+++ services/product/src/main/java/com/rkisuru/product/product/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
 
